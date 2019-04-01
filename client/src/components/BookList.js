65e38d25fa86bc43adb5735c7ba4bbde00f45b31
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-04-01 17:06:59 -0500

    client folder addes

---
+++ client/src/components/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class BookList extends Component {
+  render() {
+    return (
+      <div>
+        <ul id="book">
+          <li>Books One</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
